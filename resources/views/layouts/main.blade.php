df62ca065d0d4291dfa62fec835dac5c7df6c3e5
Jodi <jodicoetzee@live.com>
2021-01-26 15:48:44 +0200

    views

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -4,7 +4,12 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
+    <title>{{ config('app.name')}}</title>
+
+    @include('layouts.partials.header-scripts')
   </head>
   <body>
+    @yield('content')
+    @include('layouts.partials.footer-scripts')
   </body>
 </html>
