831cd1a2292bf35adc9f18c43502c451cf32a84c
Jodi <jodicoetzee@live.com>
2021-01-27 21:08:19 +0200

    updates

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -9,7 +9,7 @@
           <h5 class="text-uppercase">MovieViewerParadise</h5>
 
           <p>
-            View the best movies anywhere
+            View any information about movies anywhere
           </p>
         </div>
         <!--Grid column-->
@@ -18,20 +18,7 @@
         <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
           <h5 class="text-uppercase">Links</h5>
 
-          <ul class="list-unstyled mb-0">
-            <li>
-              <a href="#!" class="text-dark">Link 1</a>
-            </li>
-            <li>
-              <a href="#!" class="text-dark">Link 2</a>
-            </li>
-            <li>
-              <a href="#!" class="text-dark">Link 3</a>
-            </li>
-            <li>
-              <a href="#!" class="text-dark">Link 4</a>
-            </li>
-          </ul>
+
         </div>
         <!--Grid column-->
 
@@ -39,20 +26,7 @@
         <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
           <h5 class="text-uppercase mb-0">Links</h5>
 
-          <ul class="list-unstyled">
-            <li>
-              <a href="#!" class="text-dark">Link 1</a>
-            </li>
-            <li>
-              <a href="#!" class="text-dark">Link 2</a>
-            </li>
-            <li>
-              <a href="#!" class="text-dark">Link 3</a>
-            </li>
-            <li>
-              <a href="#!" class="text-dark">Link 4</a>
-            </li>
-          </ul>
+
         </div>
         <!--Grid column-->
       </div>
@@ -62,7 +36,7 @@
 
     <!-- Copyright -->
     <div class="text-center text-light p-3 copy-right">
-      © 2020 Copyright:
+      © 2021 Copyright:
       <a class="text-light" href="#">Something</a>
     </div>
     <!-- Copyright -->
