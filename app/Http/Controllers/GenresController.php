5fb60e0ab4136fad8c7bff85a6497f1b45f4d78e
Jodi <jodicoetzee@live.com>
2021-01-27 20:15:50 +0200

    class genre update

--- app/Http/Controllers/GenresController.php
+++ app/Http/Controllers/GenresController.php
@@ -4,13 +4,15 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Movie;
+
 class GenresController extends Controller
 {
     //
 
-    public function index(){
+    public function index($id)
+    {
+        $data = app(Movie::class)->getMovieViaGenre($id);
 
-
-        return view('genre.view');
+        return view('genre.view', ['data' => $data]);
     }
 }
--- app/Models/Movie.php
+++ app/Models/Movie.php
@@ -14,79 +14,94 @@ use DB;
 class Movie extends Model
 {
 
-    public function getPopularMovies(){
+    public function getPopularMovies()
+    {
         $baseUrl = env('TMDB_BASEURL');
         $apiKey = env('TMDB_APIKEY');
 
 
 
-        $combined = $baseUrl.'movie/popular?api_key='.$apiKey;
+        $combined = $baseUrl . 'movie/popular?api_key=' . $apiKey;
         $response = Http::get($combined)->json();
 
         return  $response;
-
     }
 
-    public function getTrendingMovies(){
+    public function getTrendingMovies()
+    {
         $baseUrl = env('TMDB_BASEURL');
         $apiKey = env('TMDB_APIKEY');
 
-        $combined = $baseUrl.'trending/movie/week?api_key='.$apiKey;
+        $combined = $baseUrl . 'trending/movie/week?api_key=' . $apiKey;
         $response = Http::get($combined)->json();
 
 
         return $response;
-
     }
 
-    public function getSingleMovie($id){
+    public function getSingleMovie($id)
+    {
         $baseUrl = env('TMDB_BASEURL');
         $apiKey = env('TMDB_APIKEY');
 
-        $combined = $baseUrl.'movie/'.$id.'?api_key='.$apiKey;
-        $response = Http::get($combined)->json();
-
-        return $response;
-    }
-
-    public function getMovieReviews($id){
-
-        $baseUrl = env('TMDB_BASEURL');
-        $apiKey = env('TMDB_APIKEY');
-
-        $combined = $baseUrl.'movie/'.$id.'/reviews?api_key='.$apiKey;
+        $combined = $baseUrl . 'movie/' . $id . '?api_key=' . $apiKey;
         $response = Http::get($combined)->json();
 
         return $response;
     }
 
-    public function updateWatchListMovie($data){
+    public function getMovieReviews($id)
+    {
+
+        $baseUrl = env('TMDB_BASEURL');
+        $apiKey = env('TMDB_APIKEY');
+
+        $combined = $baseUrl . 'movie/' . $id . '/reviews?api_key=' . $apiKey;
+        $response = Http::get($combined)->json();
+
+        return $response;
+    }
+    public function getMovieViaGenre($id)
+    {
+
+        $baseUrl = env('TMDB_BASEURL');
+        $apiKey = env('TMDB_APIKEY');
+
+        $combined = $baseUrl . 'discover/movie?api_key=' . $apiKey . 'language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_genres=' . $id;
+
+        $response = Http::get($combined)->json();
+
+        return $response;
+    }
+    public function updateWatchListMovie($data)
+    {
 
 
-          //check if record exist
-          if ($a = DB::table('watchlist')->where($data, 1)->exists()) {
+        //check if record exist
+        if ($a = DB::table('watchlist')->where($data, 1)->exists()) {
             //remove record
             DB::table('watchlist')->where($data)->delete();
 
             return false;
-        }else{
+        } else {
             //insert the record.
             DB::table('watchlist')->insert($data);
             return true;
         }
-
     }
-    public function checkWLM($data){
+    public function checkWLM($data)
+    {
         if ($a = DB::table('watchlist')->where($data, 1)->exists()) {
             return true;
         }
     }
 
-    public function getWatchListData($id){
+    public function getWatchListData($id)
+    {
 
         //get user watch list
 
-        $data = DB::table('watchlist')->select()->where('watchlist.user_id', '=' , $id)->get();
+        $data = DB::table('watchlist')->select()->where('watchlist.user_id', '=', $id)->get();
         return $data;
     }
 }
